4e0ddb5ffe527e3dffba96d9651fc094928a50fd
shonabirch <72304694+shonabirch@users.noreply.github.com>
2021-01-26 01:12:27 +0000

    updated

--- wwwroot/js/website.js
+++ wwwroot/js/website.js
@@ -1,26 +1,44 @@
 
 
 $(document).ready(function () {
-    $('#home').click(function () {
-        $('main').load('index.html');
-    })
-    $('#report').click(function () {
-        $('main').load('report.html');
-    })
-    $('#book').click(function () {
-        $('main').load('book.html');
-    })
-    $('#details').click(function () {
-        $('main').load('details.html');
-    })
-    $('#info').click(function () {
+    // moves between the pages
+    $(function(){
+        $("#navbar-placeholder").load('navbar.html');
+    });
+    $(document).on("click","#info",function(){
         $('main').load('info.html');
-    })
+    });
+    $(document).on("click","#details",function(){
+        $('main').load('details.html');
+    });
+    $(document).on("click","#book",function(){
+        $('main').load('book.html');
+    });
+    $(document).on("click","#report",function(){
+        $('main').load('report.html');
+    });
+    $(document).on("click","#home",function(){
+        $('main').load('index.html');
+    });
     $(document).on("click","#info1",function(){
-        $('main').load('info.html');            
+        $('main').load('info.html');
+    });
+    $(document).on("click","#staffDetails",function(){
+        $('main').load('staff.html');
+    });
+    $(document).on("click","#homebtn",function(){
+        $('main').load('index.html');
     });
 
+ 
 
+    // if you press yes or no on the report page
+    $(document).on("click","#yesbtn",function(){
+        $('main').load('yes.html');
+    });
+    $(document).on("click","#nobtn",function(){
+        $('main').load('no.html');
+    });
 
 
 
@@ -42,11 +60,23 @@ $(document).ready(function () {
     document.getElementById("outemail").innerText= email
     document.getElementById("outnumber").innerText= phone
     
-    
     })
+
+    
 });
 
-
-
+// login
+function check(form)/*function to check userid & password*/
+{
+ /*the following code checkes whether the entered userid and password are matching*/
+ if(form.userid.value == "user" && form.pswrd.value == "password")
+  {
+    $('main').load('staffview.html');/*opens the target page while Id & password matches*/
+  }
+ else
+ {
+   alert("Error Password or Username")/*displays error message*/
+  }
+}
 
 
